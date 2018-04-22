26a65f81d873290fb43e14bba030b5f486e893ce
Robert Górnicki <github@gorki.net.pl>
2018-04-22 18:08:21 +0200

    Added working gallery arrows.

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
+var currentImageIndex = 0;
+let imagePaths = ['./images/cat1.jpg', './images/cat2.jpeg', './images/cat3.jpg'];
+
+const changeImage = (event) => {
+    let image = document.getElementById('image');
+    let direction = event.target.dataset.direction;
+    if (direction == 'left' && currentImageIndex != 0) {
+        currentImageIndex--;
+    }
+    if (direction == 'right' && currentImageIndex != imagePaths.length - 1) {
+        currentImageIndex++;
+    }
+    image.src = imagePaths[currentImageIndex];
+
+    changePointer(currentImageIndex);
+};
+
+const changePointer = (index) => {
+    if (index == 0) {
+        document.getElementsByClassName('arrow-left')[0].style.cursor = 'not-allowed';
+    } else {
+        document.getElementsByClassName('arrow-left')[0].style.cursor = '';
+    }
+    if (index == imagePaths.length - 1) {
+        document.getElementsByClassName('arrow-right')[0].style.cursor = 'not-allowed';
+    } else {
+        document.getElementsByClassName('arrow-right')[0].style.cursor = '';
+    }
+};
+
 const setActiveOnFirstElement = () => {
     let element = document.getElementsByTagName("section")[0];
     element.classList.add('active');
+
+    changePointer(currentImageIndex)
 };
 
 const hideSections = () => {
     [...document.getElementsByTagName('section')].forEach(
         (section) => {
             section.classList.remove('active');
-            console.log(section);
         }
     )
 };
@@ -21,7 +52,7 @@ const removeButtonActiveness = () => {
 };
 
 const showSection = (event) => {
-    let classList = ['projects-section-link', 'experience-section-link', 'contact-section-link'];
+    let classList = ['projects-section-link', 'gallery-section-link', 'experience-section-link', 'contact-section-link'];
 
     hideSections();
     let button = event.target;
@@ -45,6 +76,4 @@ const showSection = (event) => {
             singleButton.classList.add('active-button');
         }
     )
-
-
 };
