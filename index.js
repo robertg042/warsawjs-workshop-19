44bb62f5e52360d98eea94f60e9aaea72576444b
Robert Górnicki <github@gorki.net.pl>
2018-04-22 16:48:40 +0200

    Changed coloring and layout.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function setActiveOnFirstElement() {
+const setActiveOnFirstElement = () => {
     let element = document.getElementsByTagName("section")[0];
     element.classList.add('active');
-}
+};
 
 const hideSections = () => {
     [...document.getElementsByTagName('section')].forEach(
@@ -12,12 +12,39 @@ const hideSections = () => {
     )
 };
 
-const showSection = (event) => {
-    hideSections();
-    let section = event.target.dataset.id;
-    let elementToShow = document.getElementById(section);
-    elementToShow.classList.add('active');
-    console.log(elementToShow);
+const removeButtonActiveness = () => {
+    [...document.getElementsByClassName('active-button')].forEach(
+        (button) => {
+            button.classList.remove('active-button');
+        }
+    )
 };
 
+const showSection = (event) => {
+    let classList = ['projects-section-link', 'experience-section-link', 'contact-section-link'];
 
+    hideSections();
+    let button = event.target;
+    let section = button.dataset.id;
+    let elementToShow = document.getElementById(section);
+    elementToShow.classList.add('active');
+
+    let chosenClass;
+
+    removeButtonActiveness();
+    classList.forEach(
+        (cl) => {
+            if (button.classList.contains(cl)) {
+                chosenClass = cl;
+            }
+        }
+    );
+    let navGroup = document.getElementsByClassName(chosenClass);
+    [...navGroup].forEach(
+        (singleButton) => {
+            singleButton.classList.add('active-button');
+        }
+    )
+
+
+};
